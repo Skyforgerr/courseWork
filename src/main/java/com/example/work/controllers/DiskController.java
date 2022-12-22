9f222ff546944542929fc160b5e19be21b8b5bbf
Skyforgerr <postnikoviivan@gmail.com>
2022-12-22 22:23:37 +0300

    Added vinyl form, modified navbar

--- src/main/java/com/example/work/controllers/DiskController.java
+++ src/main/java/com/example/work/controllers/DiskController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Ivan 12.12.2022
@@ -21,5 +23,16 @@ public class DiskController {
         Iterable<Disk> disks = diskRepository.findAll();
         model.addAttribute("disks", disks);
         return "disk";
+    }
+
+    @GetMapping("/adddisk")
+    public String add(Model model){
+        return "adddisk";
+    }
+    @PostMapping("/adddisk")
+    public String newDisk(@RequestParam String name, @RequestParam int cost, @RequestParam String year, @RequestParam String lable, @RequestParam String description){
+        Disk disk = new Disk(name, cost, year, lable, description);
+        diskRepository.save(disk);
+        return "redirect:/article";
     }
 }
